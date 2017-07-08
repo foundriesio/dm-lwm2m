2bb24322b60dec3619fc7ceec61f426b043ae336
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-07-07 21:10:59 -0300

    dm-lwm2m: implement firmware update state machine
    Use update counter (similar to ACID for hawkbit) current and update to
    validate if the update needs to be reported to the LWM2M server.
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- src/lwm2m.c
+++ src/lwm2m.c
@@ -47,6 +47,23 @@ BUILD_ASSERT_MSG(sizeof(CONFIG_NET_APP_PEER_IPV4_ADDR) > 1,
 static char ep_name[ENDPOINT_LEN];
 
 static struct net_app_ctx app_ctx;
+
+/*
+ * TODO: Find a better way to handle update markers, and if possible
+ * identify a common solution that could also be shared with hawkbit
+ * or other fota-implementations (e.g. common file-system structure).
+ */
+struct update_counter {
+	int current;
+	int update;
+};
+
+typedef enum {
+	COUNTER_CURRENT = 0,
+	COUNTER_UPDATE,
+} update_counter_t;
+
+static struct k_delayed_work reboot_work;
 static struct net_mgmt_event_callback cb;
 
 NET_PKT_TX_SLAB_DEFINE(lwm2m_tx_udp, 5);
@@ -64,19 +81,96 @@ static struct net_buf_pool *data_udp_pool(void)
 
 extern struct device *flash_dev;
 
+static int lwm2m_update_counter_read(struct update_counter *update_counter)
+{
+	return flash_read(flash_dev, FLASH_AREA_APPLICATION_STATE_OFFSET,
+			update_counter, sizeof(*update_counter));
+}
+
+static int lwm2m_update_counter_update(update_counter_t type, u32_t new_value)
+{
+	struct update_counter update_counter;
+	int ret;
+
+	flash_read(flash_dev, FLASH_AREA_APPLICATION_STATE_OFFSET,
+			&update_counter, sizeof(update_counter));
+	if (type == COUNTER_UPDATE) {
+		update_counter.update = new_value;
+	} else {
+		update_counter.current = new_value;
+	}
+
+	flash_write_protection_set(flash_dev, false);
+	ret = flash_erase(flash_dev, FLASH_AREA_APPLICATION_STATE_OFFSET,
+			  FLASH_AREA_APPLICATION_STATE_SIZE);
+	flash_write_protection_set(flash_dev, true);
+	if (ret) {
+		return ret;
+	}
+
+	flash_write_protection_set(flash_dev, false);
+	ret = flash_write(flash_dev, FLASH_AREA_APPLICATION_STATE_OFFSET,
+			  &update_counter, sizeof(update_counter));
+	flash_write_protection_set(flash_dev, true);
+
+	return ret;
+}
+
+static void reboot(struct k_work *work)
+{
+	SYS_LOG_INF("Rebooting device");
+	sys_reboot(0);
+}
+
 static int device_reboot_cb(u16_t obj_inst_id)
 {
-	SYS_LOG_INF("DEVICE: REBOOT");
-	sys_reboot(0);
-	return 1;
+	SYS_LOG_INF("DEVICE: Reboot in progress");
+	k_delayed_work_submit(&reboot_work, MSEC_PER_SEC);
+
+	return 0;
 }
 
 static int firmware_update_cb(u16_t obj_inst_id)
 {
-	SYS_LOG_DBG("UPDATE");
+	struct update_counter update_counter;
+	u8_t state = lwm2m_engine_get_u8("5/0/3");
+	int ret = 0;
+
+	if (state != STATE_DOWNLOADED) {
+		SYS_LOG_ERR("Cannot execute update, firmware not downloaded");
+		return -EPERM;
+	}
+
+	SYS_LOG_DBG("Executing firmware update");
+	lwm2m_engine_set_u8("5/0/3", STATE_UPDATING);
+	lwm2m_engine_set_u8("5/0/5", RESULT_DEFAULT);
+
+	/* Bump update counter so it can be verified on the next reboot */
+	ret = lwm2m_update_counter_read(&update_counter);
+	if (ret) {
+		SYS_LOG_ERR("Failed read update counter");
+		goto cleanup;
+	}
+	SYS_LOG_INF("Update Counter: current %d, update %d",
+			update_counter.current, update_counter.update);
+	ret = lwm2m_update_counter_update(COUNTER_UPDATE,
+			update_counter.current + 1);
+	if (ret) {
+		SYS_LOG_ERR("Failed to update the update counter: %d", ret);
+		goto cleanup;
+	}
+
 	boot_trigger_ota();
-	sys_reboot(0);
-	return 1;
+
+	k_delayed_work_submit(&reboot_work, MSEC_PER_SEC);
+
+	return 0;
+
+cleanup:
+	lwm2m_engine_set_u8("5/0/3", STATE_DOWNLOADED);
+	lwm2m_engine_set_u8("5/0/5", RESULT_UPDATE_FAILED);
+
+	return ret;
 }
 
 static int firmware_block_received_cb(u16_t obj_inst_id,
@@ -88,10 +182,23 @@ static int firmware_block_received_cb(u16_t obj_inst_id,
 	u8_t downloaded;
 	int ret = 0;
 
-	/* first time in callback */
+	if (total_size > FLASH_BANK_SIZE) {
+		SYS_LOG_ERR("Artifact file size too big (%d)", total_size);
+		lwm2m_engine_set_u8("5/0/3", STATE_IDLE);
+		lwm2m_engine_set_u8("5/0/5", RESULT_NO_STORAGE);
+		return -EINVAL;
+	}
+
+	/* Erase bank 1 before starting the write process */
 	if (bytes_written == 0) {
-		/* update firmware state */
-		lwm2m_engine_set_u8("5/0/3", STATE_DOWNLOADING);
+		flash_write_protection_set(flash_dev, false);
+		ret = flash_erase(flash_dev, FLASH_AREA_IMAGE_1_OFFSET,
+					FLASH_BANK_SIZE);
+		flash_write_protection_set(flash_dev, true);
+		if (ret != 0) {
+			SYS_LOG_ERR("Failed to erase flash bank 1");
+			goto cleanup;
+		}
 	}
 
 	if (data_len > 0) {
@@ -99,26 +206,38 @@ static int firmware_block_received_cb(u16_t obj_inst_id,
 					&bytes_written, data, data_len,
 					last_block);
 		if (ret < 0) {
-			return ret;
+			SYS_LOG_ERR("Failed to write flash block");
+			goto cleanup;
 		}
 	}
 
 	/* display a % downloaded, if it's different */
-	downloaded = bytes_written * 100 / total_size;
+	if (total_size) {
+		downloaded = bytes_written * 100 / total_size;
+	} else {
+		/* Total size is empty when there is only one block */
+		downloaded = 100;
+	}
+
 	if (downloaded > percent_downloaded) {
 		percent_downloaded = downloaded;
 		SYS_LOG_INF("%d%%", percent_downloaded);
 	}
 
-	/* reset out state vars on the last block */
 	if (last_block) {
-		/* update firmware state */
-		lwm2m_engine_set_u8("5/0/3", STATE_DOWNLOADED);
 		bytes_written = 0;
 		percent_downloaded = 0;
 	}
 
-	return 1;
+	return ret;
+
+cleanup:
+	bytes_written = 0;
+	percent_downloaded = 0;
+	lwm2m_engine_set_u8("5/0/3", STATE_IDLE);
+	lwm2m_engine_set_u8("5/0/5", RESULT_INTEGRITY_FAILED);
+
+	return ret;
 }
 
 static int lwm2m_setup(void)
@@ -150,6 +269,9 @@ static int lwm2m_setup(void)
 
 	/* IPSO: Temperature Sensor object */
 	lwm2m_engine_create_obj_inst("3303/0");
+
+	/* Reboot work, used when executing update */
+	k_delayed_work_init(&reboot_work, reboot);
 
 	return 0;
 }
@@ -232,10 +354,76 @@ cleanup:
 	net_app_release(&app_ctx);
 }
 
+/* TODO: Make it generic, and if possible sharing with hawkbit via library */
+static int lwm2m_image_init(void)
+{
+	int ret = 0;
+	struct update_counter counter;
+	u8_t boot_status;
+
+	/* Update boot status and update counter */
+	ret = lwm2m_update_counter_read(&counter);
+	if (ret) {
+		SYS_LOG_ERR("Failed read update counter");
+		return ret;
+	}
+	SYS_LOG_INF("Update Counter: current %d, update %d",
+			counter.current, counter.update);
+	boot_status = boot_status_read();
+	SYS_LOG_INF("Current boot status %x", boot_status);
+	if (boot_status == BOOT_STATUS_ONGOING) {
+		boot_status_update();
+		SYS_LOG_INF("Updated boot status to %x", boot_status_read());
+		ret = boot_erase_flash_bank(FLASH_AREA_IMAGE_1_OFFSET);
+		if (ret) {
+			SYS_LOG_ERR("Flash bank erase at offset %x: error %d",
+					FLASH_AREA_IMAGE_1_OFFSET, ret);
+			return ret;
+		}
+		SYS_LOG_DBG("Erased flash bank at offset %x",
+				FLASH_AREA_IMAGE_1_OFFSET);
+		if (counter.update != -1) {
+			ret = lwm2m_update_counter_update(COUNTER_CURRENT,
+						counter.update);
+			if (ret) {
+				SYS_LOG_ERR("Failed to update the update "
+					    "counter: %d", ret);
+				return ret;
+			}
+			ret = lwm2m_update_counter_read(&counter);
+			if (ret) {
+				SYS_LOG_ERR("Failed read update counter");
+				return ret;
+			}
+			SYS_LOG_INF("Update Counter updated");
+		}
+	}
+
+	/* Check if a firmware update status needs to be reported */
+	if (counter.update != -1 &&
+			counter.current == counter.update) {
+		/* Successful update */
+		SYS_LOG_INF("Firmware updated successfully");
+		lwm2m_engine_set_u8("5/0/5", RESULT_SUCCESS);
+	} else if (counter.update > counter.current) {
+		/* Failed update */
+		SYS_LOG_INF("Firmware failed to be updated");
+		lwm2m_engine_set_u8("5/0/5", RESULT_UPDATE_FAILED);
+	}
+
+	return ret;
+}
+
 int lwm2m_init(void)
 {
 	struct net_if *iface;
 	int ret;
+
+	ret = lwm2m_image_init();
+	if (ret < 0) {
+		SYS_LOG_ERR("Failed to setup image properties (%d)", ret);
+		return ret;
+	}
 
 	ret = lwm2m_setup();
 	if (ret < 0) {
